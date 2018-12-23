0c9ecc1e1bd453879af00929b8cd5e9fc0fb66d6
QwerUser2018 <44443043+QwerUser2018@users.noreply.github.com>
2018-12-23 21:48:27 +0200

    Update controller_records.php

--- app/controllers/controller_records.php
+++ app/controllers/controller_records.php
@@ -6,3 +6,29 @@ function action_addcat(){
     redirect("/");
     return "";
 }
+function action_delcat(){
+    $catname=$_GET["catName"];
+    rec_delCat($catname);
+    return redirect("/");
+}
+
+function action_viewcat(){
+    $catname=$_GET["catName"];
+    putCurrentCatIntoSession($catname);
+    rec_showCat();
+    return redirect("/");
+}
+function action_addfile(){
+    $filename=$_POST["file_name"];
+    $filevalue=$_POST["key"];
+    rec_addFile($filename,$filevalue);
+    return redirect("/");
+}
+function action_delfile(){
+    $filename=$_GET["fileName"];
+    rec_delFile($filename);
+    return redirect("/");
+
+
+
+}
